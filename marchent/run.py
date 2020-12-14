8ee3d022ac0f98da7eceb8ac419e7482f5b2633d
ndiamant <ndiamant@broadinstitute.org>
2020-12-13 22:41:40 -0500

    code works

--- marchent/run.py
+++ marchent/run.py
@@ -14,27 +14,20 @@ from marchent.board import (
 if __name__ == '__main__':
     h = 500
     w = 500
-    n = 5
+    n = 2
     marchers = [Marcher(
         w // n * i, w // n * j,
         color=int(255 * (i * j) / n**2),
         state_transitions=np.array([
             [.4, .5, .1],  # run, split, stop
-            [.4, .5, .1],
-            [.4, .5, .1],
-            [.4, .5, .1],
         ]),
         move_transitions=np.array([
-            [.0, .5, .5, .0],  # left, right, down, up
-            [.5, .0, .5, .0],
-            [.5, .0, .0, .5],
-            [.0, .5, .0, .5],
+            [.25, .25, .25, .25],  # left, right, down, up
         ]),
         )
         for i in range(1, n) for j in range(1, n)
     ]
-    state_lens = range(1, 100)
-    states = sum([state_len * 5 * [state_len % 4] for state_len in state_lens], [])
+    states = [0] * 10000
     board = Board(
         width=w,
         height=h,
